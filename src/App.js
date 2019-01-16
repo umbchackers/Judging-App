43446b69281bb8c9471c259c5b2469166ca316ba
Connor Markwell <markwell.connor@gmail.com>
2019-01-16 18:38:39 -0500

    #1: Add Table component, app styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Table from './Table/Table';
+
+const mockCSV = '11,12,13\n21,22,23\n31,32,33';
+const mockXLSX = '';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.fileInput = React.createRef();
+    this.state = {
+      tableData: mockCSV,
+    };
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Selected file - ${this.fileInput.current.files[0].name}`);
+
+    let reader = new FileReader();
+    reader.onload(file => {
+    });
   }
 
   render() {
     return (
       <div className="app">
         <form className="form-file" onSubmit={this.handleSubmit}>
-          <input type="file" accept=".csv,.xlsx" ref={this.fileInput} />
-          <input type="Submit" value="Submit" />
+          <input type="file" accept=".csv,.tsv,.xlsx" ref={this.fileInput} />
+          <input type="submit" value="Submit" />
         </form>
+        <Table data={this.state.tableData} />
       </div>
     );
   }
---
+++ src/Table/Table.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Table.css';
+
+function renderTable(data) {
+  let table = [];
+  let rows = data.split('\n');
+  for (let i = 0; i < rows.length; i++) {
+    let children = [];
+    let columns = rows[i].split(',');
+    for (let j = 0; j < columns.length; j++) {
+      children.push(<td>{columns[j]}</td>);
+    }
+    table.push(<tr>{children}</tr>);
+  }
+
+  return table;
+}
+
+function Table(props) {
+  return (
+    <table className="table">
+      {renderTable(props.data)}
+    </table>
+  );
+}
+
+export default Table;
