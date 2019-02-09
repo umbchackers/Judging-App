23b8e01c257c26159a24da506446aac8d644fecb
Connor Markwell <markwell.connor@gmail.com>
2019-02-09 02:08:32 -0500

    #2 Change proxy location

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Express middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 app.get('/api/ping', (req, res) => {
   res.send({express: 'pong!'});
+});
+
+app.post('/api/auth', (req, res) => {
+  console.log(req.body);
 });
