50ea2cdb076d1e5252a04c2b6ec589f51b07e531
Connor Markwell <markwell.connor@gmail.com>
2019-02-09 02:39:40 -0500

    #2 Add basic login route

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Load environment variables in development mode
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
 // Express middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
+// Quick little endpoint to test if API is operational
 app.get('/api/ping', (req, res) => {
-  res.send({express: 'pong!'});
+  res.send({data: 'pong!'});
 });
 
-app.post('/api/auth', (req, res) => {
-  console.log(req.body);
+// Used by client to authenticate godmode users
+app.post('/api/login', (req, res) => {
+  let status = req.body.password === process.env.GODMODE_PASSWORD ? 200 : 401;
+  res.status(status).send();
 });
