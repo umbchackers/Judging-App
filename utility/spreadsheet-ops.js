25963c0e0b81f796a497184d6cb5dfc27b2a96f7
Connor Markwell <markwell.connor@gmail.com>
2019-03-17 02:54:48 -0400

    #2: add judge name checking with Sheets API

--- utility/spreadsheet-ops.js
+++ utility/spreadsheet-ops.js
@@ -1,36 +1,54 @@
 const { google } = require('googleapis');
 const key = require('./client_secret.json');
 
-// Sheets API authorization WITH SERVICE ACCOUNT
-let jwtClient = new google.auth.JWT(
-  key.client_email, 
-  null, 
-  key.private_key,
-  ['https://www.googleapis.com/auth/spreadsheets']
-);
-
-jwtClient.authorize((err, tokens) => {
-  const authMessage = err || 'Successfully connected';
-  console.log(authMessage);
-});
-
+let jwtClient;
 let sheets = google.sheets('v4');
 
-// Spreadsheet Operations
-function isJudge(email) {
+/**
+ * Authorize Google Sheets usage
+ */
+function authorize() {
+
+  jwtClient = new google.auth.JWT(
+    key.client_email, 
+    null, 
+    key.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets']
+  );
+
+  return new Promise((resolve, reject) => {
+    jwtClient.authorize((error, token) => {
+      resolve(error === null);
+    });
+  });  
+}
+
+/**
+ * Validate email existance on judge sheet
+ * @param {string} email - email to be validated
+ */
+async function isJudge(email) {
+
+  let authed = await authorize();
+  if (!authed) { throw 'Sheets not authorized'; }
+
   sheets.spreadsheets.values.get({
     auth: jwtClient,
-    spreadsheetId: '',
-    range: 'judges:A',
-  }), (err, res) => {
-    /*
-    for each email in response
-      if email is target email 
-        return true!
-    */
-  }
-
-  return false;
+    spreadsheetId: process.env.SPR_ID,
+    range: 'judges!A'
+  }, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      let emails = response.data.values;
+      for (let i = 0; i < emails.length; i++) {
+        if (emails[i].includes(email)) {
+          return true;
+        }
+      }
+      return false;
+    }
+  });
 }
 
 module.exports = {
