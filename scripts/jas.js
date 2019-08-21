68744918a25e134e6125558d120f4f4a0e940afd
Connor Markwell <markwell.connor@gmail.com>
2019-08-21 02:33:33 -0400

    #4: Move code out of main, fix assignment code

--- scripts/jas.js
+++ scripts/jas.js
@@ -2,28 +2,32 @@ require('dotenv').config(); // Populate process.env with environment vars
 
 const spro = require('../utility/spreadsheet-ops');
 
+const args = process.argv.slice(2);
+
+async function assignProjectsToJudges() {
+  const visits = parseInt(args[0]);
+  const projects = await spro.getProjectList();
+  const judges = await spro.getJudgeList();
+  const groups = judges.map(judge => ({ judge, projects: [] }));
+  let judge = 0;
+  for (let visit = 0; visit < visits; visit++) {
+    projects.forEach(project => {
+      const projs = groups[judge].projects;
+      while (projs.includes(project)) judge++;
+      groups[judge].projects.push(project);
+      judge = (judge + 1) % judges.length;
+    });
+  }
+  return groups;
+}
+
 /**
  * 1. Get list of project titles from raw-devpost
  * 2. Get list of judges from judges
  * 3. Divide up using args
  */
-function main() {
-  const args = process.argv.slice(2);
-  const visits = parseInt(args[0]);
-  let pPromise = spro.getProjectList();
-  let jPromise = spro.getJudgeList();
-  Promise.all([pPromise, jPromise]).then(([projects, judges]) => {
-    judges = judges.map(judge => ({ judge, projects: [] }));
-    let judge = 0;
-    for (let visit = 0; visit < visits; visit++) {
-      projects.forEach(project => {
-        const proj = judges[judge].projects;
-        while (proj.includes(project)) judge++;
-        judges[judge].projects.push(project);
-        judge = (judge + 1) % judges.length;
-      });
-    }
-  });
+async function main() {
+  const groups = await assignProjectsToJudges();
 }
 
 main();
