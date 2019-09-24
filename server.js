ccbdf98a8d374bfc16d9a8039673a9fedf7c284c
Jason <Flux0s@users.noreply.github.com>
2019-09-24 14:58:21 -0400

    accidently replaced quotes

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-if (process.env.NODE_ENV != "production") {
-    require("dotenv").config();
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config();
 }
-const jwt = require("jsonwebtoken");
-const path = require("path");
+const jwt = require('jsonwebtoken');
+const path = require('path');
 
-const spro = require("./utility/spreadsheet-ops");
+const spro = require('./utility/spreadsheet-ops');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const api = require("./routes/api");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const api = require('./routes/api');
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 // JSON Parsing middleware for POST requests
@@ -22,10 +22,10 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api", api);
+app.use('/api', api);
 
 /** Return user object associated with signed JWT */
-app.get("/user/me", (req, res) => {
+app.get('/user/me', (req, res) => {
     const token = req.cookies.access_token;
     let user;
     try {
@@ -37,24 +37,24 @@ app.get("/user/me", (req, res) => {
 });
 
 /** Sign a JWT and send it as a cookie to the browser */
-app.post("/login", async (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const auth = await spro.isJudge(username, password);
     if (auth) {
         const token = jwt.sign({ username }, process.env.JWT_SECRET);
-        res.cookie("access_token", token, { httpOnly: true });
+        res.cookie('access_token', token, { httpOnly: true });
     }
     res.status(auth ? 200 : 401).send({ auth });
 });
 
 /** Remove the access_token from the browser's cookies */
-app.post("/logout", async (req, res) => {
+app.post('/logout', async (req, res) => {
     // Send delete cookie command to client
     // ...
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
