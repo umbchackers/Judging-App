f4ea18fe8dd1b5a782148344f9afe6ddd3dc6aea
Connor Markwell <markwell.connor@gmail.com>
2019-09-27 04:38:16 -0400

    #14 add cards and some styling, needs ranking functionality

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { Card, CardDeck } from 'react-bootstrap';
+
 import './Home.css';
 
-const renderCards = assignments => {
-  const cards = [];
-  assignments.forEach(assignment => {
-    cards.push(assignment);
-  });
-  return cards
+const cardStyle = {
+  minWidth: '333px', 
+  maxWidth: '333px',
+  minHeight: 'calc(20vh - 20px)',
+  maxHeight: 'calc(20vh - 20px)', 
+  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+  border: 'none',
 };
 
-const Home = ({ assignments }) => (
-  <div className="home">
-    {renderCards(assignments)}
-  </div>
-)
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  renderCards = () => {
+    const cards = [];
+    this.props.assignments.forEach(a => {
+      cards.push(
+        <Card style={cardStyle} key={a}>
+          <Card.Body>
+            <Card.Title>{a}</Card.Title>
+          </Card.Body>
+        </Card>
+      );
+    });
+    return <CardDeck>{cards}</CardDeck>
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="gallery">
+          {this.renderCards()}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
