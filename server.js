619859254ad3772c3d904ae028f52f7d41099101
Connor Markwell <markwell.connor@gmail.com>
2019-09-28 21:33:00 -0400

    #14 use dotenv when not in prod

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-//if (process.env.NODE_ENV != 'production') {
-require('dotenv').config();
-//}
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config();
+}
 const jwt = require('jsonwebtoken');
 const path = require('path');
 
